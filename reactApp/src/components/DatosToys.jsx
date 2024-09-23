2f0be6018ae6ba1a7c2cd37d6e2eccbef032fb1b
SainexDo <ventizgene@gmail.com>
2024-09-22 23:24:09 -0600

    Leve mejora en visualizacion de datos.

--- reactApp/src/components/DatosToys.jsx
+++ reactApp/src/components/DatosToys.jsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from 'react'
-// import { getDataToys } from '../server/Toys/Toys'  
-// import { getDataUsers } from '../server/Users/Users';
-import { postToys } from '../server/Toys/Toys';
+import { getDataUsers } from '../server/Users/Users';
 import { postUsers } from '../server/Users/Users';
 
 const DatosToys = () => {
@@ -13,34 +11,33 @@ const DatosToys = () => {
   const [inpMail, setInpMail] = useState('')
   const [inpPassword, setInpPassword] = useState('')
 
-  useEffect(  () => {
-
+  useEffect(() => {
+    mostrar()
   }, []);
 
   const envUsers = async () => {
     postUsers({name: inpName, age: inpAge, mail: inpMail, password: inpPassword})
   }
 
+  const mostrar = async () => {
+    const data = await getDataUsers();
+    setUsers(data) 
+  }
+
   return (
     <>
       <h1>Users List</h1>   
-        {/* {users.map((user) => ( //Este es para apis con un objeto dentro de un array.
-          <div key={user.id}>
-            <h2>id: {user.id}</h2>
-            <h2>name: {user.name}</h2>
-            <h2>age: {user.age}</h2>
+      <div id='containerUserCards'>
+        {users.map((user) => ( //Este es para apis con un objeto dentro de un array.
+            <div id='userCard' key={user.id}>
+            <div>id: {user.id}</div>
+            <div>name: {user.name}</div>
+            <div>age: {user.age}</div>
+            <div>mail: {user.mail}</div>
+            <div>password: {user.password}</div>
           </div>
-        ))} */}
-        <h1>Toys List</h1>
-      <div>
-        {/* {toys.map((toy) => ( //Este es para apis con solo arrays.
-          <div key={toy}>
-            <div id='toy'>{toy}</div>
-          </div>
-        ))} */}
+        ))}
       </div>
-      {/* <input type="text" value={newToy}  onChange={(e) => setNewToy(e.target.value)} placeholder="Enter a new toy" /> */}
-      
       <div>
         <input type="text" value={inpName}  onChange={(e) => setInpName(e.target.value)} placeholder="NAME" />
         <input type="text" value={inpAge}  onChange={(e) => setInpAge(e.target.value)} placeholder="AGE" />
--- reactApp/src/server/Users/Users.jsx
+++ reactApp/src/server/Users/Users.jsx
@@ -4,7 +4,7 @@ const getDataUsers = async () => {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    return data.users // Hay que traer de una vez el nombre del array, PERROS!!!
+    return data // Hay que traer de una vez el nombre del array, PERROS!!!
   } catch (error) {
     console.error("Error fetching data:", error);
   }
@@ -21,11 +21,9 @@ const postUsers = async (newUser) => {
       },
       body: JSON.stringify({ name: newUser.name, age: newUser.age, mail: newUser.mail, password: newUser.password }),
     });
-
     if (!response.ok) {
       throw new Error('Error en la solicitud POST');
     }
-
     const data = await response.json();
     console.log(data);
   } catch (error) {
